f8679921c85612bd742b668fc3bdc30e04291f12
romofel <felipe@romofel.dev>
2023-11-29 11:15:00 -0600

    message content that icon was clicked

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,9 +1,15 @@
 console.log("background script loaded");
 
 // listen for clicks on the extension icon
-chrome.action.onClicked.addListener((res) => {
-  console.log("icon clicked");
-  console.log({ res });
+chrome.action.onClicked.addListener(async (res) => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  if (!tab.id) throw new Error("Could not find active tab");
+  void chrome.tabs.sendMessage(tab.id, {
+    active: true,
+  });
 });
 
 // selection communication
