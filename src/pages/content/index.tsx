1f34f244161f526cffd8c75eab7b51492b6d936e
Harris Butler <harris.butler@auctane.com>
2023-11-29 15:15:58 -0600

    here we go

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Content } from "./content";
 const div = document.createElement("div");
 div.id = "__root";
 document.body.appendChild(div);
@@ -6,7 +7,7 @@ document.body.appendChild(div);
 const rootContainer = document.querySelector("#__root");
 if (!rootContainer) throw new Error("Can't find Options root element");
 const root = createRoot(rootContainer);
-root.render(<div>content script loaded</div>);
+root.render(<Content />);
 
 try {
   console.log("content script loaded");
