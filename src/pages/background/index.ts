f07cf618b52cf0bf08e5e87f27a00ba3700dfb56
romofel <felipe@romofel.dev>
2023-11-30 12:55:34 -0600

    remove all the old channels of communication

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -11,31 +11,3 @@ chrome.action.onClicked.addListener(async (res) => {
     active: true,
   });
 });
-
-// selection communication
-chrome.runtime.onConnect.addListener((port) => {
-  console.log("Awaiting Transmissions");
-
-  port.onMessage.addListener((msg) => {
-    console.log("Transmission Received");
-    console.log({ msg });
-  });
-});
-
-// wizard communication
-const wizardComms = async () => {
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  if (!tab.id) throw new Error("Could not find active tab");
-  void chrome.tabs.sendMessage(tab.id, {
-    wizard: true,
-  });
-};
-
-// this catches the wizard initiation
-chrome.runtime.onMessage.addListener((request, sender, sentdResponse) => {
-  // talk to the content
-  void wizardComms();
-});
