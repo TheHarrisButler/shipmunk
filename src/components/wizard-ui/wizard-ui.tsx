f3ba5907e2a9a8c3409e3e6fd6b2532d64c2a45e
romofel <felipe@romofel.dev>
2023-11-30 13:16:08 -0600

    display data for testing

--- src/components/wizard-ui/wizard-ui.tsx
+++ src/components/wizard-ui/wizard-ui.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 
 export const WizardUI = () => {
-  const [wizardData, setWizardData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const handleClick = (event) => {
       const clickedElement = event.target;
       const text = clickedElement.textContent || clickedElement.innerText;
 
-      setWizardData((data) => [...data, text]);
+      setData((data) => [...data, text]);
     };
     document.addEventListener("click", handleClick);
     () => document.removeEventListener("click", handleClick);
   }, []);
-  return <div>Wizard UI</div>;
+
+  return (
+    <div>
+      <h1>Wizard UI</h1>
+      {data}
+    </div>
+  );
 };
