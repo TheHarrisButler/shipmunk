48a0c11accb6b2a5e642a9b0b2a9b2762bf57dad
romofel <felipe@romofel.dev>
2023-11-30 14:00:20 -0600

    go through steps and give option to "eject"

--- src/components/wizard-ui/wizard-ui.tsx
+++ src/components/wizard-ui/wizard-ui.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export const WizardUI = ({ handleSubmit }) => {
   const [data, setData] = useState([]);
   const [step, setStep] = useState(1);
+
+  const formRef = useRef(null);
 
   useEffect(() => {
     const handleClick = (event) => {
@@ -15,34 +17,83 @@ export const WizardUI = ({ handleSubmit }) => {
     () => document.removeEventListener("click", handleClick);
   }, []);
 
+  const nextHandler = () => setStep(step + 1);
+
   return (
     <div>
       <h1>Welcome to the Label Wizard UI</h1>
       <form
+        ref={formRef}
         onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(formRef.current);
+
           handleSubmit();
         }}
       >
         {
           {
-            1: <StepOne />,
-            2: <StepTwo />,
-            3: <StepThree />,
+            1: <StepOne next={nextHandler} />,
+            2: <StepTwo next={nextHandler} />,
+            3: <StepThree next={nextHandler} />,
           }[step]
         }
+        <button type="submit">
+          {/* update button text */}
+          Eject
+        </button>
       </form>
     </div>
   );
 };
 
-const StepOne = () => {
-  return <div>StepOne</div>;
+const StepOne = ({ next }) => {
+  return (
+    <div>
+      <h2>Step One</h2>
+      <input
+        type="text"
+        name="stepone"
+        id="stepone"
+        placeholder="Input Step One"
+      />
+      <button type="button" onClick={() => next()}>
+        Next
+      </button>
+    </div>
+  );
 };
 
-const StepTwo = () => {
-  return <div>StepTwo</div>;
+const StepTwo = ({ next }) => {
+  return (
+    <div>
+      <h2>Step Two</h2>
+      <input
+        type="text"
+        name="steptwo"
+        id="steptwo"
+        placeholder="Input Step Two"
+      />
+      <button type="button" onClick={() => next()}>
+        Next
+      </button>
+    </div>
+  );
 };
 
-const StepThree = () => {
-  return <div>StepThree</div>;
+const StepThree = ({ next }) => {
+  return (
+    <div>
+      <h2>Step Three</h2>
+      <input
+        type="text"
+        name="stepthree"
+        id="stepthree"
+        placeholder="Input Step Three"
+      />
+      <button type="button" onClick={() => next()}>
+        Next
+      </button>
+    </div>
+  );
 };
